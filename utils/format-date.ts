59a34e54bccd5f559aa8ebf7fcced440e938f677
Dallas Hoffman <dallashoff@gmail.com>
2025-06-05 01:03:34 -0400

    Remove unused util

--- utils/format-date.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(
-	dateStr: string,
-	dateStyle: 'short' | 'full' | 'long' | 'medium' = 'short',
-) {
-	const date = new Date(dateStr + 'T00:00:00.000-05:00');
-	const formatter = new Intl.DateTimeFormat(undefined, { dateStyle });
-	return formatter.format(date);
-}
