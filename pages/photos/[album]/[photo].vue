397b28ea374dc0caf0b3188961766823468b0b57
Dallas Hoffman <dallashoff@gmail.com>
2025-06-07 00:13:30 -0400

    Photo lightbox

--- pages/photos/[album]/[photo].vue
+++ pages/photos/[album]/[photo].vue
@@ -7,6 +7,10 @@ const {
 	getPhotoId,
 } = await usePhotoAlbum();
 
+usePageMeta({
+	title: photo.value?.title,
+});
+
 const photoIndex = computed(() => {
 	return album.photos.findIndex((p) => p === photo.value);
 });
@@ -20,9 +24,14 @@ const nextPhotoPath = computed(() => {
 	return getPhotoPath(photo);
 });
 
-usePageMeta({
-	title: photo.value?.title,
-});
+const imgContainer = useTemplateRef('img-container');
+const ratioX = computed(() => photo.value?.ratio[0] ?? 1);
+const ratioY = computed(() => photo.value?.ratio[1] ?? 1);
+
+function toggleLightbox() {
+	const container = imgContainer.value;
+	!container?.open ? container?.showModal() : container.close();
+}
 </script>
 
 <template>
@@ -44,19 +53,26 @@ usePageMeta({
 					<fa-icon icon="fa-light fa-right"></fa-icon>
 				</nuxt-link>
 			</div>
-			<nuxt-img
-				v-if="photo"
-				:src="getPhotoSrc(photo)"
-				:alt="photo.title"
-				:width="photo.width"
-				:height="photo.height"
-				densities="1x 2x"
-				:placeholder="[photo.ratio[0] * 2, photo.ratio[1] * 2]"
-				format="auto"
-				provider="cloudflare"
-				class="page-photos-photo__img"
-				:style="{ 'view-transition-name': getPhotoId(photo) }"
-			></nuxt-img>
+			<dialog
+				ref="img-container"
+				closedby="any"
+				class="page-photos-photo__img-container"
+			>
+				<nuxt-img
+					v-if="photo"
+					:src="getPhotoSrc(photo)"
+					:alt="photo.title"
+					:width="photo.width"
+					:height="photo.height"
+					densities="1x 2x"
+					:placeholder="[ratioX * 2, ratioY * 2]"
+					format="auto"
+					provider="cloudflare"
+					class="page-photos-photo__img"
+					:style="{ 'view-transition-name': getPhotoId(photo) }"
+					@click="toggleLightbox"
+				></nuxt-img>
+			</dialog>
 		</nuxt-layout>
 	</div>
 </template>
@@ -65,6 +81,9 @@ usePageMeta({
 @use 'buttons';
 
 .page-photos-photo {
+	--photo-ratio-x: v-bind(ratioX);
+	--photo-ratio-y: v-bind(ratioY);
+	--other-content-height: 12rem;
 	margin-bottom: 3rem;
 
 	&__nav {
@@ -90,12 +109,36 @@ usePageMeta({
 		}
 	}
 
+	&__img-container {
+		display: block;
+		position: relative;
+		width: 100%;
+		max-width: calc(
+			(100vh - var(--other-content-height)) *
+				(var(--photo-ratio-x) / var(--photo-ratio-y))
+		);
+		max-height: none;
+		margin: auto;
+		border: none;
+		background: none;
+		cursor: zoom-in;
+		transition: max-width 400ms;
+
+		&:open {
+			--other-content-height: 0px;
+			cursor: zoom-out;
+		}
+
+		&::backdrop {
+			background-color: rgb(0 0 0 / 0.5);
+		}
+	}
+
 	&__img {
 		display: block;
 		width: 100%;
-		max-width: calc((100vh - 10rem) * (4 / 3));
 		height: auto;
-		margin: 0 auto;
+		margin: auto;
 	}
 
 	@media (min-width: 400px) {
